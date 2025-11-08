6e54546907c26124bd15cc7264ca59bb1864cf21
yhnapoleon <90117490+yhnapoleon@users.noreply.github.com>
2025-11-08 01:55:32 +0000

    YH

--- ex1.py
+++ ex1.py
@@ -1,3 +1,15 @@
 def find(s, n):
-# write your implementation here
+    l=len(s)
+    for i in range(l):
+        for j in range(i+1,l):
+            if s[i]+s[j]==n:
+                print(s[i],s[j])
+                return (i,j)
+
+
     return None
+
+if __name__ == "__main__":
+    s=[10,15,3,7]
+    n=17
+    result=find(s,n)
